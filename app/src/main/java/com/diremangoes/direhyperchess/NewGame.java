0a439cdd1681080f6e60ab86cf38ea8d851b3700
Steven Rohr <srohr3792@gmail.com>
2016-02-06 03:02:53 -0500

    such push. wow

--- app/src/main/java/com/diremangoes/direhyperchess/NewGame.java
+++ app/src/main/java/com/diremangoes/direhyperchess/NewGame.java
@@ -1,15 +1,26 @@
 package com.diremangoes.direhyperchess;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
 import java.net.Socket;
 
 public class NewGame extends AppCompatActivity {
+    private ServerSocket serverSocket;
+    Handler updateConversationHandler;
+    Thread serverThread = null;
+    private TextView text;
+    public static final int SERVERPORT = 6000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,9 +38,86 @@ public class NewGame extends AppCompatActivity {
             }
         });
 
+        text = (TextView) findViewById(R.id.text2);
+        text.setText("Launching server");
+        updateConversationHandler = new Handler();
+
+        this.serverThread = new Thread(new ServerThread());
+        this.serverThread.start();
+
 
     }
 
-   
+    @Override
+    protected void onStop() {
+        super.onStop();
+        try {
+            serverSocket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    class ServerThread implements Runnable {
+        public void run() {
+            Socket socket = null;
+            try {
+                serverSocket = new ServerSocket(SERVERPORT);
+                text.setText("Server launched!");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            while (!Thread.currentThread().isInterrupted()) {
+                try {
+                    text.setText("Server running!");
+                    socket = serverSocket.accept();
+                    CommunicationThread commThread = new CommunicationThread(socket);
+                    new Thread(commThread).start();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    class CommunicationThread implements Runnable {
+        private Socket clientSocket;
+        private BufferedReader input;
+        public CommunicationThread(Socket clientSocket) {
+            this.clientSocket = clientSocket;
+
+            try {
+                this.input = new BufferedReader(new InputStreamReader(this.clientSocket.getInputStream()));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        public void run() {
+            while (!Thread.currentThread().isInterrupted()) {
+                try {
+                    String read = input.readLine();
+                    updateConversationHandler.post(new updateUIThread(read));
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+    }
+
+    class updateUIThread implements Runnable {
+        private String msg;
+
+        public updateUIThread(String str) {
+            this.msg = str;
+        }
+
+        @Override
+        public void run() {
+            text.setText(text.getText().toString()+"Client Says: "+ msg + "\n");
+        }
+    }
 
 }
