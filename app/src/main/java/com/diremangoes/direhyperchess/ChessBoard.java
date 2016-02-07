292c2fb1ae8f4b9e2dd272e1ed9b59d00a2e1462
Preston Hamlin <pwh11@my.fsu.edu>
2016-02-07 05:27:58 +0000

    getValidMoves mostly done

--- app/src/main/java/com/diremangoes/direhyperchess/ChessBoard.java
+++ app/src/main/java/com/diremangoes/direhyperchess/ChessBoard.java
@@ -17,29 +17,29 @@ public class ChessBoard {
         }
         // add white pawns
         for (int x=0; x<8; ++x)
-            _tiles[x][0] = new Tile(PieceType.PAWN, 1, x,0);
+            _tiles[x][1] = new Tile(PieceType.PAWN, 1, x,1);
         // add white pieces
-        _tiles[0][1] = new Tile(PieceType.ROOK,   1, 0,1);
-        _tiles[1][1] = new Tile(PieceType.KNIGHT, 1, 1,1);
-        _tiles[2][1] = new Tile(PieceType.BISHOP, 1, 2,1);
-        _tiles[3][1] = new Tile(PieceType.KING,   1, 3,1);
-        _tiles[4][1] = new Tile(PieceType.QUEEN,  1, 4,1);
-        _tiles[5][1] = new Tile(PieceType.BISHOP, 1, 5,1);
-        _tiles[6][1] = new Tile(PieceType.KNIGHT, 1, 6,1);
-        _tiles[7][1] = new Tile(PieceType.ROOK,   1, 7,1);
+        _tiles[0][0] = new Tile(PieceType.ROOK,   1, 0,0);
+        _tiles[1][0] = new Tile(PieceType.KNIGHT, 1, 1,0);
+        _tiles[2][0] = new Tile(PieceType.BISHOP, 1, 2,0);
+        _tiles[3][0] = new Tile(PieceType.KING,   1, 3,0);
+        _tiles[4][0] = new Tile(PieceType.QUEEN,  1, 4,0);
+        _tiles[5][0] = new Tile(PieceType.BISHOP, 1, 5,0);
+        _tiles[6][0] = new Tile(PieceType.KNIGHT, 1, 6,0);
+        _tiles[7][0] = new Tile(PieceType.ROOK,   1, 7,0);
         
         // add black pawns
         for (int x=0; x<8; ++x)
-            _tiles[x][7] = new Tile(PieceType.PAWN, 2, x,7);
+            _tiles[x][6] = new Tile(PieceType.PAWN, 2, x,6);
         // add black pieces
-        _tiles[0][6] = new Tile(PieceType.ROOK,   2, 0,6);
-        _tiles[1][6] = new Tile(PieceType.KNIGHT, 2, 1,6);
-        _tiles[2][6] = new Tile(PieceType.BISHOP, 2, 2,6);
-        _tiles[3][6] = new Tile(PieceType.KING,   2, 3,6);
-        _tiles[4][6] = new Tile(PieceType.QUEEN,  2, 4,6);
-        _tiles[5][6] = new Tile(PieceType.BISHOP, 2, 5,6);
-        _tiles[6][6] = new Tile(PieceType.KNIGHT, 2, 6,6);
-        _tiles[7][6] = new Tile(PieceType.ROOK,   2, 7,6);
+        _tiles[0][7] = new Tile(PieceType.ROOK,   2, 0,7);
+        _tiles[1][7] = new Tile(PieceType.KNIGHT, 2, 1,7);
+        _tiles[2][7] = new Tile(PieceType.BISHOP, 2, 2,7);
+        _tiles[3][7] = new Tile(PieceType.KING,   2, 3,7);
+        _tiles[4][7] = new Tile(PieceType.QUEEN,  2, 4,7);
+        _tiles[5][7] = new Tile(PieceType.BISHOP, 2, 5,7);
+        _tiles[6][7] = new Tile(PieceType.KNIGHT, 2, 6,7);
+        _tiles[7][7] = new Tile(PieceType.ROOK,   2, 7,7);
         
         
         // print the current board
@@ -52,8 +52,15 @@ public class ChessBoard {
         
         // and verify the movement calculations
         // ArrayList<Tile> moves = getMoves(new Tile(PieceType.KNIGHT, 1, 4,4));
-        ArrayList<Tile> moves = getMoves(_tiles[1][1]);
+        ArrayList<Tile> moves;
         
+        System.out.println("moves:");
+        moves = getMoves(_tiles[1][1]);
+        for (Tile t : moves)
+            t.printInfo();
+        
+        System.out.println("valid moves:");
+        moves = getValidMoves(_tiles[1][1]);
         for (Tile t : moves)
             t.printInfo();
         
@@ -77,7 +84,7 @@ public class ChessBoard {
         // get some local names for target data
         ArrayList<Tile> ret = new ArrayList();
         PieceType ptype     = t.getPieceType();
-        int colour          = t.getPieceOwner();
+        int color           = t.getPieceOwner();
         int xpos            = t.getxpos();
         int ypos            = t.getypos();
         
@@ -93,7 +100,7 @@ public class ChessBoard {
                 int[] arr = {-1, 0, 1};
                 for (int i : arr) {
                     if (inBounds(xpos+i, ypos-1))
-                        ret.add(new Tile(ptype, colour, xpos+i, ypos-1));
+                        ret.add(new Tile(ptype, color, xpos+i, ypos-1));
                 }
                 break;
             }
@@ -101,11 +108,11 @@ public class ChessBoard {
                 // x-translations
                 for (int x=0; x<8; ++x) {
                     if (x == xpos) continue;
-                    ret.add(new Tile(ptype, colour, x, ypos));
+                    ret.add(new Tile(ptype, color, x, ypos));
                 }
                 for (int y=0; y<8; ++y) {
                     if (y == ypos) continue;
-                    ret.add(new Tile(ptype, colour, xpos, y));
+                    ret.add(new Tile(ptype, color, xpos, y));
                 }
                 break;
             }
@@ -120,22 +127,22 @@ public class ChessBoard {
                 // look in each diagonal until out of bounds
                 for (int i=1; i<8; ++i) {
                     if (inBounds(xpos+i, ypos+i))
-                        ret.add(new Tile(ptype, colour, xpos+i, ypos+i));
+                        ret.add(new Tile(ptype, color, xpos+i, ypos+i));
                     else break;
                 }
                 for (int i=1; i<8; ++i) {
                     if (inBounds(xpos+i, ypos-i))
-                        ret.add(new Tile(ptype, colour, xpos+i, ypos-i));
+                        ret.add(new Tile(ptype, color, xpos+i, ypos-i));
                     else break;
                 }
                 for (int i=1; i<8; ++i) {
                     if (inBounds(xpos-i, ypos+i))
-                        ret.add(new Tile(ptype, colour, xpos-i, ypos+i));
+                        ret.add(new Tile(ptype, color, xpos-i, ypos+i));
                     else break;
                 }
                 for (int i=1; i<8; ++i) {
                     if (inBounds(xpos-i, ypos-i))
-                        ret.add(new Tile(ptype, colour, xpos-i, ypos-i));
+                        ret.add(new Tile(ptype, color, xpos-i, ypos-i));
                     else break;
                 }
                 
@@ -157,7 +164,7 @@ public class ChessBoard {
                     for (int j : arr) {
                         if (i == j || i == -j) continue;
                         if (inBounds(xpos+i, ypos+j))
-                            ret.add(new Tile(ptype, colour, xpos+i, ypos+j));
+                            ret.add(new Tile(ptype, color, xpos+i, ypos+j));
                     }
                 }
                 
@@ -167,32 +174,32 @@ public class ChessBoard {
                 // x-translations
                 for (int x=0; x<8; ++x) {
                     if (x == xpos) continue;
-                    ret.add(new Tile(ptype, colour, x, ypos));
+                    ret.add(new Tile(ptype, color, x, ypos));
                 }
                 for (int y=0; y<8; ++y) {
                     if (y == ypos) continue;
-                    ret.add(new Tile(ptype, colour, xpos, y));
+                    ret.add(new Tile(ptype, color, xpos, y));
                 }
                     
                 // look in each diagonal until out of bounds
                 for (int i=1; i<8; ++i) {
                     if (inBounds(xpos+i, ypos+i))
-                        ret.add(new Tile(ptype, colour, xpos+i, ypos+i));
+                        ret.add(new Tile(ptype, color, xpos+i, ypos+i));
                     else break;
                 }
                 for (int i=1; i<8; ++i) {
                     if (inBounds(xpos+i, ypos-i))
-                        ret.add(new Tile(ptype, colour, xpos+i, ypos-i));
+                        ret.add(new Tile(ptype, color, xpos+i, ypos-i));
                     else break;
                 }
                 for (int i=1; i<8; ++i) {
                     if (inBounds(xpos-i, ypos+i))
-                        ret.add(new Tile(ptype, colour, xpos-i, ypos+i));
+                        ret.add(new Tile(ptype, color, xpos-i, ypos+i));
                     else break;
                 }
                 for (int i=1; i<8; ++i) {
                     if (inBounds(xpos-i, ypos-i))
-                        ret.add(new Tile(ptype, colour, xpos-i, ypos-i));
+                        ret.add(new Tile(ptype, color, xpos-i, ypos-i));
                     else break;
                 }
                 
@@ -214,7 +221,7 @@ public class ChessBoard {
                     for (int j : arr) {
                         if (i == 0 && j == 0) continue;
                         if (inBounds(xpos+i, ypos+j))
-                            ret.add(new Tile(ptype, colour, xpos+i, ypos+j));
+                            ret.add(new Tile(ptype, color, xpos+i, ypos+j));
                     }
                 }
                 
@@ -228,7 +235,187 @@ public class ChessBoard {
     
     // returns all the valid moves for a given target piece
     ArrayList<Tile> getValidMoves(Tile t) {
+        // get some local names for target data
         ArrayList<Tile> ret = new ArrayList();
+        PieceType ptype     = t.getPieceType();
+        int color           = t.getPieceOwner();
+        int ocolor          = (t.getPieceOwner() == 1 ? 2 : 1);
+        int xpos            = t.getxpos();
+        int ypos            = t.getypos();
+        
+        switch(ptype) {
+            case EMPTY: {
+                break;
+            }
+            case PAWN: {
+                int[] arr = {-1, 0, 1};
+                for (int i : arr) {
+                    if (inBounds(xpos+i, ypos-1)) {
+                        if (!hasPiece(_tiles[xpos+1][ypos-1], color))
+                            ret.add(new Tile(ptype, color, xpos+i, ypos-1));
+                    }
+                }
+                break;
+            }
+            case ROOK: {
+                // x-translations
+                for (int x=xpos+1; x<8; ++x) {
+                    if (hasPiece(_tiles[x][ypos], color)) break;
+                    ret.add(new Tile(ptype, color, x, ypos));
+                }
+                for (int x=xpos-1; x>=0; --x) {
+                    if (hasPiece(_tiles[x][ypos], color)) break;
+                    ret.add(new Tile(ptype, color, x, ypos));
+                }
+                // y-translations
+                for (int y=ypos+1; y<8; ++y) {
+                    if (hasPiece(_tiles[xpos][y], color)) break;
+                    ret.add(new Tile(ptype, color, xpos, y));
+                }
+                for (int y=ypos-1; y>=0; --y) {
+                    if (hasPiece(_tiles[xpos][y], color)) break;
+                    ret.add(new Tile(ptype, color, xpos, y));
+                }
+                break;
+            }
+            case BISHOP: {
+                // look in each diagonal until out of bounds
+                int newx, newy;
+                
+                // for each diagonal, seek until out of bounds or piece hit
+                for (int i=0; ; ++i) {
+                    newx = xpos+i;
+                    newy = ypos+i;
+                    if (!inBounds(newx, newy)) break;
+                    else if (hasPiece(_tiles[xpos][ypos], color)) break;
+                    else { // if enemy piece found, you can take it
+                        ret.add(new Tile(ptype, color, newx, newy));
+                        if (hasPiece(_tiles[xpos][ypos], ocolor)) break;
+                    }
+                }
+                for (int i=0; ; ++i) {
+                    newx = xpos+i;
+                    newy = ypos-i;
+                    if (!inBounds(newx, newy)) break;
+                    else if (hasPiece(_tiles[xpos][ypos], color)) break;
+                    else { // if enemy piece found, you can take it
+                        ret.add(new Tile(ptype, color, newx, newy));
+                        if (hasPiece(_tiles[xpos][ypos], ocolor)) break;
+                    }
+                }
+                for (int i=0; ; ++i) {
+                    newx = xpos-i;
+                    newy = ypos+i;
+                    if (!inBounds(newx, newy)) break;
+                    else if (hasPiece(_tiles[xpos][ypos], color)) break;
+                    else { // if enemy piece found, you can take it
+                        ret.add(new Tile(ptype, color, newx, newy));
+                        if (hasPiece(_tiles[xpos][ypos], ocolor)) break;
+                    }
+                }
+                for (int i=0; ; ++i) {
+                    newx = xpos-i;
+                    newy = ypos-i;
+                    if (!inBounds(newx, newy)) break;
+                    else if (hasPiece(_tiles[xpos][ypos], color)) break;
+                    else { // if enemy piece found, you can take it
+                        ret.add(new Tile(ptype, color, newx, newy));
+                        if (hasPiece(_tiles[xpos][ypos], ocolor)) break;
+                    }
+                }
+                break;
+            }
+            case KNIGHT: {
+                int[] arr = {-2, -1, 1, 2};
+                for (int i : arr) {
+                    for (int j : arr) {
+                        if (i == j || i == -j) continue;
+                        if (inBounds(xpos+i, ypos+j))
+                            if (!hasPiece(_tiles[xpos+i][ypos+j], color))
+                                ret.add(new Tile(ptype, color, xpos+i, ypos+j));
+                    }
+                }
+                break;
+            }
+            case QUEEN: {
+                // look in each diagonal until out of bounds
+                int newx, newy;
+                
+                // x-translations
+                for (int x=xpos+1; x<8; ++x) {
+                    if (hasPiece(_tiles[x][ypos], color)) break;
+                    ret.add(new Tile(ptype, color, x, ypos));
+                }
+                for (int x=xpos-1; x>=0; --x) {
+                    if (hasPiece(_tiles[x][ypos], color)) break;
+                    ret.add(new Tile(ptype, color, x, ypos));
+                }
+                // y-translations
+                for (int y=ypos+1; y<8; ++y) {
+                    if (hasPiece(_tiles[xpos][y], color)) break;
+                    ret.add(new Tile(ptype, color, xpos, y));
+                }
+                for (int y=ypos-1; y>=0; --y) {
+                    if (hasPiece(_tiles[xpos][y], color)) break;
+                    ret.add(new Tile(ptype, color, xpos, y));
+                }
+                
+                // for each diagonal, seek until out of bounds or piece hit
+                for (int i=0; ; ++i) {
+                    newx = xpos+i;
+                    newy = ypos+i;
+                    if (!inBounds(newx, newy)) break;
+                    else if (hasPiece(_tiles[xpos][ypos], color)) break;
+                    else { // if enemy piece found, you can take it
+                        ret.add(new Tile(ptype, color, newx, newy));
+                        if (hasPiece(_tiles[xpos][ypos], ocolor)) break;
+                    }
+                }
+                for (int i=0; ; ++i) {
+                    newx = xpos+i;
+                    newy = ypos-i;
+                    if (!inBounds(newx, newy)) break;
+                    else if (hasPiece(_tiles[xpos][ypos], color)) break;
+                    else { // if enemy piece found, you can take it
+                        ret.add(new Tile(ptype, color, newx, newy));
+                        if (hasPiece(_tiles[xpos][ypos], ocolor)) break;
+                    }
+                }
+                for (int i=0; ; ++i) {
+                    newx = xpos-i;
+                    newy = ypos+i;
+                    if (!inBounds(newx, newy)) break;
+                    else if (hasPiece(_tiles[xpos][ypos], color)) break;
+                    else { // if enemy piece found, you can take it
+                        ret.add(new Tile(ptype, color, newx, newy));
+                        if (hasPiece(_tiles[xpos][ypos], ocolor)) break;
+                    }
+                }
+                for (int i=0; ; ++i) {
+                    newx = xpos-i;
+                    newy = ypos-i;
+                    if (!inBounds(newx, newy)) break;
+                    else if (hasPiece(_tiles[xpos][ypos], color)) break;
+                    else { // if enemy piece found, you can take it
+                        ret.add(new Tile(ptype, color, newx, newy));
+                        if (hasPiece(_tiles[xpos][ypos], ocolor)) break;
+                    }
+                }
+                break;
+            }
+            case KING: {
+                int[] arr = {-1, 0, 1};
+                for (int i : arr) {
+                    for (int j : arr) {
+                        if (i == 0 && j == 0) continue;
+                        if (inBounds(xpos+i, ypos+j))
+                            if (!hasPiece(_tiles[xpos+i][ypos+j], color))
+                                ret.add(new Tile(ptype, color, xpos+i, ypos+j));
+                    }
+                }
+                break;
+            }
+        }
         
         return ret;
     }
